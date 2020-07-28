78c20bac869b5b87645f2cf47c575701bd3ded42
Aaron Bushnell <aaron.bushnell@gmail.com>
2020-07-27 20:59:53 -0400

    Process variety of element types

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -9,6 +9,11 @@ use craft\helpers\UrlHelper;
 use craft\web\Controller;
 use craft\web\View;
 
+use craft\base\Element;
+use craft\elements\Category;
+use craft\elements\Entry;
+use Solspace\Calendar\Elements\Event;
+
 /**
  * @author    TrendyMinds
  * @package   Visor
@@ -63,21 +68,95 @@ class DefaultController extends Controller
         // Send the value to the template to be returned as raw HTML
         $oldMode = Craft::$app->view->getTemplateMode();
         Craft::$app->view->setTemplateMode(View::TEMPLATE_MODE_CP);
-        $html = Craft::$app->view->renderTemplate('visor/visor', [ "entry" => $element ]);
+
+        // Render a template using our custom variables to determine what a user should and shouldn't see
+        $html = Craft::$app->view->renderTemplate('visor/visor', [
+            "element" => $this->_parseElement($element),
+        ]);
+
         Craft::$app->view->setTemplateMode($oldMode);
 
         return $this->asRaw($html);
+    }
 
-        // return $this->asJson([
-        //     "data" => (object) [
-        //         "referrer" => $uri,
-        //         "siteName" => Craft::$app->config->general->siteName,
-        //         "title" => $element ? $element->title : "",
-        //         "cpEditUrl" => $element ? $element->getCpEditUrl() : "",
-        //         "allEntries" => UrlHelper::cpUrl() . "/entries",
-        //         "controlPanel" => UrlHelper::cpUrl(),
-        //         "signOut" => "/" . Craft::$app->config->general->logoutPath
-        //     ]
-        // ]);
+    /**
+     * Parses a given element and determines what, if anything, to show for each property in the Visor template
+     *
+     * @param Element $element
+     *
+     * @return void
+     */
+    private function _parseElement(Element $element = null)
+    {
+        if ($element === null) {
+            return (object) [
+                "title" => "",
+                "type" => "",
+                "cpUrl" => "",
+                "editUrl" => "",
+                "isUserEditable" => false
+            ];
+        }
+
+        // Handle entries
+        if (get_class($element) === "craft\\elements\\Entry") {
+            /** @var Entry */
+            $entry = $element;
+
+            $cpUrl = UrlHelper::cpUrl() . "/entries/";
+
+            if ($entry->getSection()->type === "single") {
+                $cpUrl .= "singles";
+            }
+
+            if ($entry->getSection()->type !== "single") {
+                $cpUrl .= $entry->getSection()->handle;
+            }
+
+            return (object) [
+                "title" => $entry->title,
+                "type" => $entry->getSection()->type === "single" ? "Singles" : $entry->getSection()->name,
+                "cpUrl" => $cpUrl,
+                "editUrl" => $entry->getCpEditUrl(),
+                "isUserEditable" => Craft::$app->user->identity->can("editEntries:" . $entry->getSection()->uid)
+            ];
+        }
+
+        // Handle categories
+        if (get_class($element) === "craft\\elements\\Category") {
+            /** @var Category */
+            $category = $element;
+
+            return (object) [
+                "title" => $category->title,
+                "type" => $category->group->name,
+                "cpUrl" => UrlHelper::cpUrl() . "/categories/" . $category->group->handle,
+                "editUrl" => $category->getCpEditUrl(),
+                "isUserEditable" => Craft::$app->user->identity->can("editCategories:" . $category->group->uid)
+            ];
+        }
+
+        // Handle events
+        if (get_class($element) === "Solspace\\Calendar\\Elements\\Event") {
+            /** @var Event */
+            $event = $element;
+
+            return (object) [
+                "title" => $event->title,
+                "type" => $event->getCalendar()->name,
+                "cpUrl" => UrlHelper::cpUrl() . "/calendar/events/",
+                "editUrl" => $event->getCpEditUrl(),
+                "isUserEditable" => Craft::$app->user->identity->can("calendar-manageEventsFor:" . $event->getCalendar()->id)
+            ];
+        }
+
+        // Set defaults if we did not properly identify the element type
+        return (object) [
+            "title" => "",
+            "type" => "",
+            "cpUrl" => "",
+            "editUrl" => "",
+            "isUserEditable" => false
+        ];
     }
 }
